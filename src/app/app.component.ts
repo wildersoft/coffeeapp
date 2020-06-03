e10c6ee6df0d43471574a1885ef0904b8c67555b
wildersoft <wildersoft@gmail.com>
2020-06-02 19:19:38 -0500

    api offline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,20 @@ export class AppComponent implements OnInit {
 
   }
 
+  updateNetworkStatusUI() {
+    if (navigator.onLine) {
+      (document.querySelector("body") as any).style = "";
+    }
+    else {
+      (document.querySelector("body") as any).style = "filter: grayscale(1)";
+    }
+  }
+
   ngOnInit() {
+
+    this.updateNetworkStatusUI();
+    window.addEventListener("online", this.updateNetworkStatusUI);
+    window.addEventListener("offline", this.updateNetworkStatusUI);
     if ((navigator as any).standalone == false) {
       this.snackBar.open("You can add this PWA to the Home Screen", "", { duration : 3000 });
     }
