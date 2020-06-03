4ecddb409d1ac1722723a169a888911340381744
wildersoft <wildersoft@gmail.com>
2020-06-02 21:43:15 -0500

    adding notification supports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { SwUpdate, SwPush} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,9 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 })
 export class AppComponent implements OnInit {
   title = 'coffeeapp';
+  readonly VAPID_PUBLIC_KEY = "BLBx-hf2WrL2qEa0qKb-aCJbcxEvyn62GDTyyP9KTS5K7ZL0K7TfmOKSPqp8vQF0DaG8hpSBknz_x3qf5F4iEFo";
 
-  constructor(private snackBar : MatSnackBar) {
+  constructor(private snackBar : MatSnackBar, private updates: SwUpdate, private pushes: SwPush) {
 
   }
 
@@ -21,9 +23,36 @@ export class AppComponent implements OnInit {
       (document.querySelector("body") as any).style = "filter: grayscale(1)";
     }
   }
+  subscribeToPush() {
+    Notification.requestPermission(permision => {
+      if (permision == "granted") {
+        // this.updates.
+        this.pushes.requestSubscription({ serverPublicKey: this.VAPID_PUBLIC_KEY })
+          .then(() => {
+            console.log("notification accepted");
+            //TODO
+          });
+      }
+    })
+  }
 
   ngOnInit() {
-
+    //checking sw updates
+    this.updates.available.subscribe(event => {
+      if (event.type == "UPDATE_AVAILABLE") {
+        const sb = this.snackBar.open("There is an update available", "Install Now", { duration: 4000});
+        sb.onAction().subscribe(() => {
+          this.updates.activateUpdate().then(() => {
+            console.log("The App was updated");
+            location.reload();
+          });          
+        });
+      }
+      else {
+ 
+      }
+    })
+    //checking network status
     this.updateNetworkStatusUI();
     window.addEventListener("online", this.updateNetworkStatusUI);
     window.addEventListener("offline", this.updateNetworkStatusUI);
@@ -31,6 +60,7 @@ export class AppComponent implements OnInit {
       this.snackBar.open("You can add this PWA to the Home Screen", "", { duration : 3000 });
     }
     
+    //checking instalation status
     if ((navigator as any).standalone == undefined) {
       //It's not IOS
       if (window.matchMedia("(display-mode:broser").matches) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { SwUpdate } from '@angular/service-worker';
 
 const routes : Routes =[
   {path : '', component: ListComponent},
